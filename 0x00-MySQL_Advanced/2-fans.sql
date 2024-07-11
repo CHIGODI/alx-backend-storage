61dd30af2c539d36b542d7d3d9ed22c4ef4d2797
CHIGODI <chiegody254@gmail.com>
2024-07-11 15:16:39 +0300

    Add task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,6 @@
 -- ranks country origins of bands, ordered by the number
 -- of (non - unique) fans
 
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` from metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
