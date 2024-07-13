5fce9b7b38db128bca8a37423de8f007107fa560
CHIGODI <chiegody254@gmail.com>
2024-07-13 13:39:56 +0300

    Add deterministic keyword

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,12 +1,13 @@
 -- SQL script to create a function SafeDiv that divides (and returns) the first
 -- by the second number or returns 0 if the second number is equal to 0.
-
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT(24)
+DETERMINISTIC
 BEGIN
-    DECLARE result INT;
+    DECLARE result FLOAT(24);
 
     IF b = 0 THEN
         SET result = 0;
