15aefa468ba861e9cf07e3262959c0824d42fbe4
CHIGODI <chiegody254@gmail.com>
2024-07-13 12:43:46 +0300

    Create column first letter then create index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,8 +1,4 @@
 -- SQL script that creates an index idx_name_first on the table names
 -- and the first letter of name.
 
-ALTER TABLE names
-ADD COLUMN first_letter CHAR(1)
-SET first_letter = SUBSTR(name, 1, 1);
-
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX idx_name_first ON names (name(1));
