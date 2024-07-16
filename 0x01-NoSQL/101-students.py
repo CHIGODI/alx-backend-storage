c736778ca1a31743e537dea39a3b399f109816cb
CHIGODI <chiegody254@gmail.com>
2024-07-16 11:56:06 +0300

    use aggregate to find average and oder

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
-
 """
 Write a Python function that returns all students sorted by average score:
 
 Prototype: def top_students(mongo_collection):
 mongo_collection will be the pymongo collection object
 The top must be ordered
-The average score must be part of each item returns with key = averageScore
+The average score must be part of each item returned with key = averageScore
 """
 
+
 def top_students(mongo_collection):
-    """ """
-    mongo_collection.find()
+    """
+    Returns all students sorted by average score.
+    """
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    return list(mongo_collection.aggregate(pipeline))
