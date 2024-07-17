154cbbcbbd31c7ebd3dd4b7d2d282f07ca5f7ea4
CHIGODI <chiegody254@gmail.com>
2024-07-17 21:59:50 +0300

    Add task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,68 +1,80 @@
 #!/usr/bin/env python3
-"""
-This module contains a class Cache that makes connection to redis
-"""
 import redis
 import uuid
-from typing import Optional, Callable, Union
+from typing import Union, Callable, Optional
+from functools import wraps
 
 
-class Cache:
-    """
-    Cache class to interact with Redis for storing and retrieving data.
-    """
+def count_calls(method: Callable) -> Callable:
+    ''' def count calls '''
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        ''' def wrapper '''
+        key_name = method.__qualname__
+        self._redis.incr(key_name, 0) + 1
+        return method(self, *args, **kwds)
+    return wrapper
 
-    def __init__(self) -> None:
-        """
-        Initialize the Cache class.
-        Establishes a connection to the Redis server and clears the
-        current database.
-        """
+
+def call_history(method: Callable) -> Callable:
+    ''' def call history '''
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        ''' def wrapper'''
+        key_m = method.__qualname__
+        inp_m = key_m + ':inputs'
+        outp_m = key_m + ":outputs"
+        data = str(args)
+        self._redis.rpush(inp_m, data)
+        fin = method(self, *args, **kwds)
+        self._redis.rpush(outp_m, str(fin))
+        return fin
+    return wrapper
+
+
+def replay(func: Callable):
+    '''def replay'''
+    r = redis.Redis()
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m,
+            k.decode('utf-8'),
+            v.decode('utf-8')
+        )
+        print(fin)
+
+
+class Cache():
+    ''' class cache '''
+    def __init__(self):
+        ''' def init '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
-        """
-        Method that takes a data argument and returns a string.
-        This method generate a random key (using uuid),
-        store the input data in Redis using the random key and
-        return the key.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        ''' def store '''
+        generate = str(uuid.uuid4())
+        self._redis.set(generate, data)
+        return generate
 
-    def get(self, key: str, fn: Optional[Callable] = None)\
-            -> Union[str, int, bytes, float]:
-        """
-        Retrieve data from Redis using the provided key.
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' def get '''
+        value = self._redis.get(key)
+        return value if not fn else fn(value)
 
-        :param key: The key to retrieve data from Redis.
-        :param fn: Optional callable to convert the retrieved data.
-        :return: The converted data if fn is provided and data exists,
-                else the raw data from Redis (bytes).
-        """
-        data = self._redis.get(key)
-        if data is None:
-            return None
-        return fn(data) if fn else data
+    def get_int(self, key):
+        return self.get(key, int)
 
-    def get_str(self, key) -> Optional[str]:
-        """
-        Retrieve string data from Redis using the provided key.
-
-        :param key: The key to retrieve string data from Redis.
-        :return: The string data retrieved from Redis, or None
-        if key doesn't exist.
-        """
-        return self.get(key, fn=lambda data: data.decode('utf-8'))
-
-    def get_int(self, key) -> Optional[int]:
-        """
-        Retrieve integer data from Redis using the provided key.
-
-        :param key: The key to retrieve integer data from Redis.
-        :return: The integer data retrieved from Redis, or None
-        if key doesn't exist.
-        """
-        return self.get(key, fn=lambda data: int(data) if data else None)
+    def get_str(self, key):
+        value = self._redis.get(key)
+        return value.decode("utf-8")
