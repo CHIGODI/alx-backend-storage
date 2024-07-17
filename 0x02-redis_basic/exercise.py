bf875d9725e3af66a117bcec76bd2c57f7b7dabb
CHIGODI <chiegody254@gmail.com>
2024-07-17 19:11:44 +0300

    Add get method to search key in redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ This module contains a class Cache that makes connection to redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Optional, Callable, Any
 
 
 class Cache:
@@ -12,7 +12,7 @@ class Cache:
     Cache class to interact with Redis for storing and retrieving data.
     """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """
         Initialize the Cache class.
         Establishes a connection to the Redis server and clears the
@@ -21,7 +21,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[int, float, bytes, str]) -> str:
+    def store(self, data: Any) -> str:
         """
         Method that takes a data argument and returns a string.
         This method generate a random key (using uuid),
@@ -31,3 +31,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Any]] = None)\
+            -> Optional[Any]:
+        """
+        Retrieve data from Redis using the provided key.
+
+        :param key: The key to retrieve data from Redis.
+        :param fn: Optional callable to convert the retrieved data.
+        :return: The converted data if fn is provided and data exists,
+                else the raw data from Redis (bytes).
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
