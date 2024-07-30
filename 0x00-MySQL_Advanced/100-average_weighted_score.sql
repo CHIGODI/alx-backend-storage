5a2985b37adb09cceda7bc5b6f102d49ea9355be
CHIGODI <chiegody254@gmail.com>
2024-07-30 12:06:37 +0300

    Add task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,18 +3,36 @@
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN p_user_id INT);
-
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN p_user_id INT)
 BEGIN
-    DECLARE avg_w FLOAT;
+    DECLARE weighted_sum FLOAT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
+    DECLARE weighted_average FLOAT;
 
-    SELECT AVG(score) INTO avg_w
-    FROM corrections
-    WHERE user_id = p_user_id;
+    -- Calculate the weighted sum of scores and total weight
+    SELECT
+        SUM(c.score * p.weight),
+        SUM(p.weight)
+    INTO
+        weighted_sum,
+        total_weight
+    FROM
+        corrections c
+    JOIN
+        projects p ON c.project_id = p.id
+    WHERE
+        c.user_id = p_user_id;
 
-UPDATE users
-SET average_score = avg_w
-WHERE id = p_user_id;
+    -- Calculate the weighted average
+    SET weighted_average = weighted_sum / total_weight;
 
-END;
+    -- Update the users table with the calculated weighted average
+    UPDATE
+        users
+    SET
+        average_score = weighted_average
+    WHERE
+        id = p_user_id;
+END$$
+
 DELIMITER ;
